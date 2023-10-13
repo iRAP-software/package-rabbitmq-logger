caca3f9d666651ee11f34e1b5a63310847bfac3d
SamratRai <samrat.rai@irap.org>
2023-10-13 17:36:25 +0100

    RabbitMQ connection created only when logging
    - PHP >= 8.0 requirement added

--- src/RabbitmqQueueLogger.php
+++ src/RabbitmqQueueLogger.php
@@ -9,126 +9,170 @@
 
 namespace iRAP\RabbitmqLogger;
 
-class RabbitmqQueueLogger implements \iRAP\Logging\LoggerInterface
+use Exception;
+use iRAP\Logging\LoggerInterface;
+use iRAP\Logging\LogLevel;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class RabbitmqQueueLogger implements LoggerInterface
 {
-    private $m_queueName;
-    private $m_channel;
-    private $m_source = 'n/s';
-    
-     /**
+    private string $m_queueName;
+    private ?AMQPChannel $m_channel = null;
+    private string $m_source = 'n/s';
+    private array $m_params;
+
+    /**
      * Create the RabbitmqQueueLogger.
      * @param string $host - the host where the RabbitMQ server is.
      * @param string $user - the username to connect to RabbitMQ with.
      * @param string $password - the password to connect with.
      * @param string $queueName - the name of the queue to publish to.
      * @param int $port - optional - the port of the server. Defaults to 5672.
-     * @param string $source - optional - the name of the project that is using this library
+     * @param string|null $source - optional - the name of the project that is using this library
+     * @throws Exception
      */
-    public function __construct($host, $user, $password, $queueName, $port=5672, $source=null) 
+    public function __construct(string $host, string $user, string $password, string $queueName, int $port = 5672, string $source = null)
     {
         $this->m_queueName = $queueName;
-        
-        $connection = new \PhpAmqpLib\Connection\AMQPStreamConnection(
-            $host, 
-            $port, 
-            $user, 
-            $password
-        );
-        
-        $this->m_channel = $connection->channel();
-        
-        $this->m_channel->queue_declare(
-            $queueName,
-            $passive = false,
-            $durable = true,
-            $exclusive = false,
-            $auto_delete = false,
-            $nowait = false,
-            $arguments = null,
-            $ticket = null
-        );
-        
+        $this->m_params = [
+            'host' => $host,
+            'port' => $port,
+            'user' => $user,
+            'password' => $password
+        ];
+
         // if the project constant is defined
-        if(defined("SERVICE_NAME")) {
+        if (defined("SERVICE_NAME")) {
             $this->m_source = SERVICE_NAME;
         }
         // overwrite the value if one is actually passed to the object
-        if($source) {
+        if ($source) {
             $this->m_source = $source;
         }
 
     }
-    
-    public function alert($message, array $context = array()) 
+
+    /**
+     * @throws Exception
+     */
+    public function alert($message, array $context = array()): void
     {
-        $this->log(\iRAP\Logging\LogLevel::ALERT, $message, $context);
+        $this->log(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical($message, array $context = array()) 
+    /**
+     * @throws Exception
+     */
+    public function critical($message, array $context = array()): void
     {
-        $this->log(\iRAP\Logging\LogLevel::CRITICAL, $message, $context);
+        $this->log(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function debug($message, array $context = array()) 
+    /**
+     * @throws Exception
+     */
+    public function debug($message, array $context = array()): void
     {
-        $this->log(\iRAP\Logging\LogLevel::DEBUG, $message, $context);
+        $this->log(LogLevel::DEBUG, $message, $context);
     }
 
-    public function emergency($message, array $context = array()) 
+    /**
+     * @throws Exception
+     */
+    public function emergency($message, array $context = array()): void
     {
-        $this->log(\iRAP\Logging\LogLevel::EMERGENCY, $message, $context);
+        $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function error($message, array $context = array()) 
+    /**
+     * @throws Exception
+     */
+    public function error($message, array $context = array()): void
     {
-        $this->log(\iRAP\Logging\LogLevel::ERROR, $message, $context);
+        $this->log(LogLevel::ERROR, $message, $context);
     }
 
-    public function info($message, array $context = array()) 
+    /**
+     * @throws Exception
+     */
+    public function info($message, array $context = array()): void
     {
-        $this->log(\iRAP\Logging\LogLevel::INFO, $message, $context);
+        $this->log(LogLevel::INFO, $message, $context);
     }
-    
-    
+
+    /**
+     * @throws Exception
+     */
+    public function notice($message, array $context = array()): void
+    {
+        $this->log(LogLevel::NOTICE, $message, $context);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function warning($message, array $context = array()): void
+    {
+        $this->log(LogLevel::WARNING, $message, $context);
+    }
+
+
     /**
      * Log something. This is the "base" method used by all other methods in this class.
      * @param int $level - the level of the log, higher is more urgent. Use \iRAP\Logging\LogLevel
      * @param string $message - message to log.
      * @param array $context - array of any extra context to log
+     * @throws Exception
      */
-    public function log($level, $message, array $context = array()) 
+    public function log($level, $message, array $context = array()): void
     {
         # If contextString is not JSON encodable, then just get a print_r 
         # representation of it.
-        if (is_null(json_encode($context)))
-        {
+        if (is_null(json_encode($context))) {
             $context = print_r($context, true);
         }
-                
+
         $logArray = array(
             'level' => $level,
             'timestamp' => time(),
             'message' => $message,
             'context' => $context,
             'source' => $this->m_source
-        );      
-        
-        $msg = new \PhpAmqpLib\Message\AMQPMessage(
+        );
+
+        $msg = new AMQPMessage(
             json_encode($logArray, JSON_UNESCAPED_SLASHES),
             array('delivery_mode' => 2) # make message persistent
         );
-        
-        $this->m_channel->basic_publish($msg, '', $this->m_queueName);
-    }
-    
-    
-    public function notice($message, array $context = array()) 
-    {
-        $this->log(\iRAP\Logging\LogLevel::NOTICE, $message, $context);
+
+        $this->getChannel()->basic_publish($msg, '', $this->m_queueName);
     }
 
-    public function warning($message, array $context = array()) 
+    /**
+     * @throws Exception
+     */
+    private function getChannel(): AMQPChannel
     {
-        $this->log(\iRAP\Logging\LogLevel::WARNING, $message, $context);
+        static $connection = null;
+
+        if ($connection === null) {
+            $connection = new AMQPStreamConnection(...$this->m_params);
+
+            $this->m_channel = $connection->channel();
+
+            $this->m_channel->queue_declare(
+                $this->m_queueName,
+                false,
+                true,
+                false,
+                false,
+                false,
+                null
+            );
+        }
+
+        return $this->m_channel;
     }
 }
